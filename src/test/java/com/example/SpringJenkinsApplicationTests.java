bed2302e417f49edb440cd0e1b4b245234fe3712
SowmyaReddy02 <127983629+SowmyaReddy02@users.noreply.github.com>
2023-12-22 10:53:04 -0500

    Update SpringJenkinsApplicationTests.java

--- src/test/java/com/example/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/SpringJenkinsApplicationTests.java
@@ -1,18 +1,2 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-
-
-@SpringBootTest
-class SpringJenkinsApplicationTests {
-	
-
-	@Test
-	void contextLoads() {
-		
-	}
-
-}

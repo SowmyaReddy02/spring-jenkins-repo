cbfa1b089fcdd67f5bc14da96714ff2eb2d5362d
SowmyaReddy02 <127983629+SowmyaReddy02@users.noreply.github.com>
2023-12-22 10:49:46 -0500

    Update SpringJenkinsApplicationTests.java

--- src/test/java/com/example/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/SpringJenkinsApplicationTests.java
@@ -8,16 +8,13 @@ import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
-	public Logger logger = org.slf4j.LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
-		logger.info("Test case executing..");
-		assertEquals(true, true);
+		
 	}
 
 }

d2adc27187da3144237b1c29ab30541941581ce6
SowmyaReddy02 <127983629+SowmyaReddy02@users.noreply.github.com>
2023-12-22 10:51:36 -0500

    Update SpringJenkinsApplicationTests.java

--- src/test/java/com/example/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/SpringJenkinsApplicationTests.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
